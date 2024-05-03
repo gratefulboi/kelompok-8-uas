2098a84423a6198a6e2a6251232699c69935f953
ArrathG <saduady@gmail.com>
2024-05-03 17:59:59 +0700

    Push 2

--- src/pages/Wisata.jsx
+++ src/pages/Wisata.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
-import dToba from '../assets/danauToba.jpeg';
+import danauToba from '../assets/danauToba.jpeg';
 
 // Function to generate a set of boxes
 function BoxSet({ imageSrc, text1, text2, text3 }) {
   return (
     <>
       {/* First box */}
-      <div className="container mx-auto mt-4 flex">
-        <div className="w-full bg-gray-200 border border-gray-400 relative" style={{ paddingBottom: '37.5%' }}>
+      <div className="container mx-auto mt-4 relative">
+        <div className="w-full bg-gray-200 border border-gray-400" style={{ paddingBottom: '37.5%' }}>
           {/* Content of the first box */}
           <img src={imageSrc} alt="" className="absolute top-0 left-0 w-full h-full object-cover" />
+          {/* Text on the photo */}
+          <div className="absolute bottom-0 left-0 p-4">
+            <p className="text-white text-lg font-bold">{text1}</p>
+          </div>
         </div>
       </div>
       {/* Second box */}
       <div className="container mx-auto mt-4 flex">
         <div className="w-3/4 p-4 bg-blue-200 border border-blue-400 mr-4">
           {/* Content of the second box */}
-          <p>{text1}</p>
+          <p>{text2}</p>
         </div>
         {/* Third box */}
         <div className="w-1/4 p-4 bg-green-200 border border-green-400">
           {/* Content of the third box */}
-          <p>{text2}</p>
+          <p>{text3}</p>
         </div>
       </div>
       {/* Fourth box */}
       <div className="container mx-auto mt-4 flex">
         <div className="w-1/3 p-4 bg-yellow-200 border border-yellow-400 mr-4">
           {/* Content of the fourth box */}
-          <p>{text3}</p>
+          <p>This is the content of the fourth box.</p>
         </div>
         {/* Fifth box */}
         <div className="w-2/3 p-4 bg-purple-200 border border-purple-400">
@@ -47,20 +51,20 @@ export default function Wisata() {
       <h1 className="flex justify-center bg-red-600 mt-2">Page Wisata</h1>
       
       {/* Call the function to display the first set of boxes with dynamic content */}
-      <div className="mb-8"> {/* Add margin bottom to create space between sets */}
+      <div className="mb-12"> {/* Add larger margin bottom to create more space between sets */}
         <BoxSet 
-          imageSrc={dToba}
-          text1="Danau Toba (Batak Toba: Surat Batak: ᯖᯀᯬ ᯖᯬᯅ, translit. Tao Toba) adalah danau alami berukuran besar di Indonesia yang terletak di kaldera gunung supervulkan."
+          imageSrc={danauToba}
+          text1="Text on the photo"
           text2="Content of the third box."
           text3="This is the content of the fourth box."
         />
       </div>
       
       {/* Call the function again to display the second set of boxes with different content */}
-      <div className="mb-8"> {/* Add margin bottom to create space between sets */}
+      <div className="mb-12"> {/* Add larger margin bottom to create more space between sets */}
         <BoxSet 
-          imageSrc={dToba}
-          text1="Different text for the second set."
+          imageSrc={danauToba}
+          text1="Different text on the photo"
           text2="Another content for the third box."
           text3="Modified content for the fourth box."
         />
