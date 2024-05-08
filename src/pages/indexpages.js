2461a8d0ff91e95823d6399f1124fe4ca182706b
gratefulboi <bryanyung2005@gmail.com>
2024-05-08 18:00:16 +0700

    Masukkin src yg udh bener
    Please work

---
+++ src/pages/indexpages.js
@@ -0,0 +1,5 @@
+export { default as AboutUs } from './Aboutus';
+export { default as Budaya } from './Budaya';
+export { default as Home } from './Home';
+export { default as Kuliner } from './Kuliner';
+export { default as Wisata } from './Wisata';
