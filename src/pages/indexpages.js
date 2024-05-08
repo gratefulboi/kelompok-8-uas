16d40acc3b7fbe067d6bf57c3e84ccde58690ec8
gratefulboi <bryanyung2005@gmail.com>
2024-05-08 17:36:42 +0700

    Ngapus pages

--- src/pages/indexpages.js
+++
@@ -1,5 +0,0 @@
-export { default as AboutUs } from './Aboutus';
-export { default as Budaya } from './Budaya';
-export { default as Home } from './Home';
-export { default as Kuliner } from './Kuliner';
-export { default as Wisata } from './Wisata';
