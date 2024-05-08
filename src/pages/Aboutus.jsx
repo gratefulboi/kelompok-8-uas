81f258848457251c93d98fb48627f2d94e4180f8
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2024-05-08 18:17:18 +0700

    8-5-24
    18:17

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
-import '../index.css'
-import Bryan from '../assets/Profile/Bryan.jpeg'
-import Sadu from '../assets/Profile/Sadu.jpeg'
+import { Link } from 'react-router-dom';
+import '../index.css';
+import Bryan from '../assets/Profile/Bryan.jpeg';
+import Sadu from '../assets/Profile/Sadu.jpeg';
+import Instagram from '../assets/Icons/Instagram.svg';
+import X from '../assets/Icons/X.svg';
+import Fb from '../assets/Icons/Fb.svg';
+import Yt from '../assets/Icons/Yt.svg';
 
 import { FiUser, FiStar } from 'react-icons/fi';
 
@@ -11,7 +15,7 @@ export default function AboutUs() {
         <div className="relative min-h-screen">
             <div className="absolute inset-0 bg-gradient-animation h-full w-full"></div>
             <div className="relative z-10">
-                <div className="h-64 min-h-screen py-12"data-aos ="fade-up">
+                <div className="h-64 min-h-screen py-12" data-aos="fade-up">
                     <h1 className="font-Unna text-yellow-50 text-3xl font-bold mb-6 text-center py-4">The Founder</h1>
                     <hr className="card w-1/3 mx-auto border-b-2 border-black opacity-70 mb-8" />
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
@@ -41,18 +45,40 @@ export default function AboutUs() {
                         />
                     </div>
                 </div>
-                <div className="bg-gradient-to-r from-trueGray-900 to-black animate-gradient-preference py-8"data-aos ="fade-up">
-                    <div className="max-w-6xl mx-auto">
-                        <h2 className="text-4xl font-Unna text-yellow-50 mb-4">References</h2>
-                        <div className="text-yellow-50 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                            <ReferenceItem title="Sumber" description="Indahnya negriku" />
-                            <ReferenceItem title="Kontak" description="" />
-                            <ReferenceItem title="10 Dosa besar Jerome" description="Saham Kalbe bangkrut setelah colab sama jerome." />
-                            <ReferenceItem title="Siapa kita?" description="Goyang nasi padang pakai sambal randang" />
+                <div className="bg-gradient-to-r from-trueGray-900 to-black animate-gradient-preference py-4 mx-auto flex justify-center items-center" data-aos="fade-up">
+                    <div className="max-w-6xl mx-auto px-8">
+                        <div className="text-yellow-50  p-1 flex flex-col md:flex-row justify-start items-start">
+                            <div className="text-justify w-5/12 mr-20">
+                                <ReferenceItem title="Djedjak Soematra" description="Kami dasdsamilih membuat website dengan tema Sumatera Utara, karena menurut kami provinsi ini memiliki banyak keberagaman yang dapat dikunjungi oleh para wisatawan" />
+                            </div>
+                            <div className="h-8 my-auto w-2/12 mr-4">
+                                <Link to="/Budaya">
+                                    <ReferenceItem title="Budaya"  />
+                                </Link>
+                            </div>
+                            <div className="h-8 my-auto w-2/12 mr-4">
+                                <Link to="/Wisata">
+                                    <ReferenceItem title="Wisata"  />
+                                </Link>
+                            </div>
+                            <div className="h-8 my-auto w-2/12">
+                                <Link to="/Kuliner">
+                                    <ReferenceItem title="Kuliner" />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="text-yellow-50 mt-4">
+                            <p className="text-sm">&copy; 2024 YourWebsite. All rights reserved.</p>
+                        </div>
+                        <div className="w-fit flex mt-3 items-center max-w-6xl">
+                            <img src={Instagram} alt="Instagram" className="cursor-pointer mr-2 rounded-full p-1 w-10 h-10 icon-shadow icon" />
+                            <img src={X} alt="X" className="cursor-pointer rounded-full mr-2 p-1 w-10 h-10 icon-shadow icon" />
+                            <img src={Fb} alt="Facebook" className="cursor-pointer rounded-full mr-2 p-1 w-10 h-10 icon-shadow icon" />
+                            <img src={Yt} alt="YouTube" className="cursor-pointer rounded-full p-1 w-10 h-10 icon-shadow icon" />
                         </div>
                     </div>
-                    <div className="text-center text-yellow-50 mt-8">
-                        <p className="text-sm">&copy; 2024 YourWebsite. All rights reserved.</p>
+                    <div className="mb-8 flex items-center justify-center text-m text-center text-yellow-50 mt-8">
+                        <p></p>
                     </div>
                 </div>
             </div>
@@ -72,11 +98,12 @@ const PersonCard = ({ name, role, description, imageSrc }) => (
 );
 
 const ReferenceItem = ({ title, description }) => (
-    <div className="card border border-solid p-4 rounded-lg shadow-md">
-        <h3 className="text-lg font-semibold mb-2 flex items-center text-yellow-50">
-            <FiStar className="mr-1" /> {/* Icon */}
+    <div className=" p-0">
+        <h3 className={`text-xl font-semibold mb-4 flex items-center text-yellow-50 ${title === 'Budaya' || title === 'Wisata' || title === 'Kuliner' ? 'hover:text-yellow-400' : ''}`}>
             {title}
         </h3>
         <p className="text-yellow-50 font-rubik">{description}</p>
     </div>
 );
+
+
