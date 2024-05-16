c0ea503659a35d3cb88b762e2772351f0da24089
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2024-05-16 20:14:04 +0700

    16-5-24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 import { Link } from 'react-router-dom';
 
-
 const ReferenceItem = ({ title, description }) => (
-    <div className=" p-0">
+    <div className="p-0">
         <h3 className={`text-xl font-semibold mb-4 flex items-center text-yellow-50 ${title === 'Budaya' || title === 'Wisata' || title === 'Kuliner' ? 'hover:text-yellow-400' : ''}`}>
             {title}
         </h3>
@@ -14,31 +13,30 @@ export default function Footer() {
     return (
         <div className="bg-gradient-to-r from-trueGray-900 to-black animate-gradient-preference py-2 mx-auto flex justify-center items-center">
             <div className="max-w-6xl mx-auto px-8">
-                <div className="text-yellow-50  p-1 flex flex-col md:flex-row justify-start items-start">
-                    <div className="text-justify w-8/12 mr-20">
-                        <ReferenceItem title="Djedjak Soemoet" description="Sumatera Utara, terletak  pada bagian utara Pulau Sumatera Indonesia, dengan Medan sebagai ibu kotanya. Keindahan alam yang memukau, serta warisan budaya dari macam-macam suku membuat Sumatera Utara sangat menarik untuk dijelajahi." />
+                <div className="text-yellow-50 p-1 flex flex-col md:flex-row md:justify-between md:items-center">
+                    <div className="text-justify text-sm lg:text-lg w-full md:w-7/12">
+                        <ReferenceItem title="Djedjak Soemoet" description="Sumatera Utara, terletak pada bagian utara Pulau Sumatera Indonesia, dengan Medan sebagai ibu kotanya. Keindahan alam yang memukau, serta warisan budaya dari macam-macam suku membuat Sumatera Utara sangat menarik untuk dijelajahi." />
                     </div>
-                    <div className="h-16 my-auto w-2/12 ml-8">
-                        <Link to="/Budaya">
-                            <ReferenceItem title="Budaya"  />
-                        </Link>
-                    </div>
-                    <div className="h-16 my-auto w-2/12 ml-10">
-                        <Link to="/Wisata">
-                            <ReferenceItem title="Wisata"  />
-                        </Link>
-                    </div>
-                    <div className="h-16 my-auto w-2/12 ml-8">
-                        <Link to="/Kuliner">
-                            <ReferenceItem title="Kuliner" />
-                        </Link>
+                    <div className="flex flex-col md:flex-row md:justify-end w-full md:w-5/12 mt-4 md:mt-0">
+                        <div className="h-10 lg:h-16 w-auto mb-0 md:mb-0 md:mr-20">
+                            <Link to="/Budaya">
+                                <ReferenceItem title="Budaya" />
+                            </Link>
+                        </div>
+                        <div className="h-10 w-auto mb-0 md:mb-0 md:mr-20">
+                            <Link to="/Wisata">
+                                <ReferenceItem title="Wisata" />
+                            </Link>
+                        </div>
+                        <div className="h-16 w-auto">
+                            <Link to="/Kuliner">
+                                <ReferenceItem title="Kuliner" />
+                            </Link>
+                        </div>
                     </div>
                 </div>
-                <div className="text-yellow-50 mt-3">
+                <div className="text-yellow-50 mt-0 lg:mt-3"> 
                     <p className="text-sm">&copy; 2024 YourWebsite. All rights reserved.</p>
-                </div>
-                <div className="w-fit flex mt-3 items-center max-w-6xl">
-                    
                 </div>
             </div>
             <div className="mb-8 flex items-center justify-center text-m text-center text-yellow-50 mt-8">
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -4,20 +4,15 @@ import Bryan from '../assets/Profile/Bryan.jpeg';
 import Sadu from '../assets/Profile/Sadu.jpeg';
 import Jerikho from '../assets/Profile/Jerikho.jpeg';
 import Kerby from '../assets/Profile/Kerby.jpeg';
-import Instagram from '../assets/Icons/Instagram.svg';
-import X from '../assets/Icons/X.svg';
-import Fb from '../assets/Icons/Fb.svg';
-import Yt from '../assets/Icons/Yt.svg';
 
 export default function AboutUs() {
     return (
-        <div className="relative min-h-screen">
-            <div className="absolute inset-0 bg-gradient-animation h-full w-full"></div>
-            <div className="relative z-10">
-                <div className="h-64 min-h-screen py-12" data-aos="fade-up">
-                    <h1 className="font-Unna text-yellow-50 text-3xl font-bold mb-6 text-center py-4">The Founder</h1>
-                    <hr className="card w-1/3 mx-auto border-b-2 border-black opacity-70 mb-8" />
-                    <div className="text-justify grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
+        <div className="flex justify-center items-center relative min-h-screen absolute bg-gradient-animation h-full w-full">
+            <div className="min-h-screen py-12 text-justify" data-aos="fade-up">
+                <h1 className="font-Unna text-yellow-50 text-3xl font-bold mb-6 text-center py-4">The Founder</h1>
+                <hr className="card w-1/3 mx-auto border-b-2 border-black opacity-70 mb-8" />
+                <div className="flex flex-col items-center justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
                         <PersonCard 
                             name="Bryan Yung"
                             role="Leader"
@@ -50,12 +45,14 @@ export default function AboutUs() {
 }
 
 const PersonCard = ({ name, role, description, imageSrc }) => (
-    <div className="relative cursor-pointer card rounded-lg shadow-md overflow-hidden border-4 border-transparent animate-marching-ants duration-100 ease-linear">
-        <img className="w-full h-40 overflow-hidden transition-transform duration-300 transform hover:scale-110 object-cover" src={imageSrc} alt={name} />
+    <div className="w-full sm:w-3/4 md:w-full relative cursor-pointer card rounded-lg shadow-md overflow-hidden border-4 border-transparent animate-marching-ants duration-100 ease-linear">
+        <div className='overflow-hidden'>
+            <img className="w-full h-50 lg:h-40 overflow-hidden transition-transform duration-300 transform hover:scale-110 object-cover" src={imageSrc} alt={name} />
+        </div>
         <div className="p-4">
-            <h2 className="text-lg text-yellow-50 font-Unna">{name}</h2>
-            <p className="font-serif text-yellow-50 text-base text-gray-600 mb-2">{role}</p>
-            <p className="text-sm text-yellow-50">{description}</p>
+            <h2 className="text-2xl lg:text-xl text-yellow-50 font-Unna">{name}</h2>
+            <p className="font-serif text-yellow-50 text-xl lg:text-base text-gray-600 mb-2">{role}</p>
+            <p className="text-base lg:text-sm  text-yellow-50">{description}</p>
         </div>
     </div>
 );
