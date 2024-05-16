22ac2c4410e8a622e3bbbb2c08bd99acafdfbc97
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2024-05-16 21:45:50 +0700

    16-5-24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
         <div className="bg-gradient-to-r from-trueGray-900 to-black animate-gradient-preference py-2 mx-auto flex justify-center items-center">
             <div className="max-w-6xl mx-auto px-8">
                 <div className="text-yellow-50 p-1 flex flex-col md:flex-row md:justify-between md:items-center">
-                    <div className="text-justify text-sm lg:text-lg w-full md:w-7/12">
+                    <div className="text-justify text-sm lg:text-lg w-full lg:w-7/12">
                         <ReferenceItem title="Djedjak Soemoet" description="Sumatera Utara, terletak pada bagian utara Pulau Sumatera Indonesia, dengan Medan sebagai ibu kotanya. Keindahan alam yang memukau, serta warisan budaya dari macam-macam suku membuat Sumatera Utara sangat menarik untuk dijelajahi." />
                     </div>
                     <div className="flex flex-col md:flex-row md:justify-end w-full md:w-5/12 mt-4 md:mt-0">
@@ -23,19 +23,19 @@ export default function Footer() {
                                 <ReferenceItem title="Budaya" />
                             </Link>
                         </div>
-                        <div className="h-10 w-auto mb-0 md:mb-0 md:mr-20">
+                        <div className="h-10 w-auto mb-0 lg:mb-0 lg:mr-20">
                             <Link to="/Wisata">
                                 <ReferenceItem title="Wisata" />
                             </Link>
                         </div>
-                        <div className="h-16 w-auto">
+                        <div className="h-10 w-auto">
                             <Link to="/Kuliner">
                                 <ReferenceItem title="Kuliner" />
                             </Link>
                         </div>
                     </div>
                 </div>
-                <div className="text-yellow-50 mt-0 lg:mt-3"> 
+                <div className="text-yellow-50 lg:mt-3"> 
                     <p className="text-sm">&copy; 2024 YourWebsite. All rights reserved.</p>
                 </div>
             </div>
