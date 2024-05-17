81649b5b795f6968f20179bd9913dc5e805f5cf8
gratefulboi <bryanyung2005@gmail.com>
2024-05-17 12:52:35 +0700

    Update budaya lebih responsive
    DONEEEE

--- src/pages/Budaya.jsx
+++ src/pages/Budaya.jsx
@@ -99,32 +99,23 @@ export default function Budaya() {
           </CardContent>
         </Card>
 
-        <Card className='m-10 flex w-11/12 rounded-2xl bg-krem shadow-xl' data-aos="fade-left">
-          <CardContent className='m-3 lg:m-10 flex flex-col justify-end items-end ml-auto w-1/2'>
-            <CardTitle className='text-base lg:text-3xl items-end ml-auto text-right'>Mandok Hata</CardTitle>
-            <p className='items-end ml-auto text-justify text-xs lg:text-base'>Mandok Hata merupakan salah satu tradisi dari suku Batak. Mandok Hata berasal dari kata Mandok yang berarti menyampaikan dan Hata yang berarti kata. 
-              Mandok Hata sendiri memiliki pengertian penyampaian sepatah dua patah kata yang dilakukan seseorang di acara tertentu. Umumnya Mandok Hata ini dilakukan saat perayaan malam tahun baru. Mandok Hata yang dilakukan pada 
-              perayaan malam tahun baru, dimana semua keluarga berkumpul baik dari kalangan muda sampai tua. Mandok Hata memiliki tujuan sebagai ucapan syukur, permohonan maaf atas apa yang terjadi selama setahun kebelakang, serta 
-              mengungkapkan harapan kedepan supaya menjadi lebih baik kedepan.
-            </p>
+        <Card className='m-10 flex flex-col lg:flex-row w-11/12 rounded-2xl bg-krem shadow-xl' data-aos="fade-down" data-aos-anchor-placement="right-bottom">
+          <div className='overflow-hidden lg:w-1/2 rounded-t-2xl rounded-bl-none lg:rounded-bl-2xl lg:rounded-tr-none flex-grow'>
+            <img src={gambarMandokhata} className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out w-full h-full object-cover' alt="Gambar Mandok Hata" />
+          </div>
+          <CardContent className='m-3 lg:m-10 flex flex-col justify-end items-start ml-auto lg:w-1/2'>
+            <CardTitle className='text-base lg:text-3xl items-start mr-auto text-left'>Mandok Hata</CardTitle>
+            <p className='items-end ml-auto text-justify text-xs lg:text-base'>Mandok Hata merupakan salah satu tradisi dari suku Batak. Mandok Hata berasal dari kata Mandok yang berarti menyampaikan dan Hata yang berarti kata. Mandok Hata sendiri memiliki pengertian penyampaian sepatah dua patah kata yang dilakukan seseorang di acara tertentu. Umumnya Mandok Hata ini dilakukan saat perayaan malam tahun baru. Mandok Hata yang dilakukan pada perayaan malam tahun baru, dimana semua keluarga berkumpul baik dari kalangan muda sampai tua. Mandok Hata memiliki tujuan sebagai ucapan syukur, permohonan maaf atas apa yang terjadi selama setahun kebelakang, serta mengungkapkan harapan kedepan supaya menjadi lebih baik kedepan.</p>
           </CardContent>
-          <div className='overflow-hidden w-1/2 rounded-tr-2xl rounded-br-2xl flex-grow'>
-              <img src={gambarMandokhata} className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out w-full h-full object-cover'/>
-          </div>
         </Card>
-      
-        <Card className='m-10 flex w-11/12 rounded-2xl bg-krem shadow-xl' data-aos="fade-right">
-          <div className='overflow-hidden w-1/2 rounded-tl-2xl rounded-bl-2xl flex-grow'>
-              <img src={gambarLompatbatu} className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out w-full h-full object-cover'/>
+
+        <Card className='m-10 flex flex-col lg:flex-row w-11/12 rounded-2xl bg-krem shadow-xl' data-aos="fade-down">
+          <div className='overflow-hidden lg:w-1/2 rounded-t-2xl rounded-bl-none lg:rounded-br-2xl lg:rounded-tl-none flex-grow lg:order-2'>
+            <img src={gambarLompatbatu} className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out w-full h-full object-cover' alt="Gambar Fahombo" />
           </div>
-          <CardContent className='m-3 lg:m-10 flex flex-col justify-start items-start ml-auto w-1/2'>
-            <CardTitle className='text-base lg:text-3xl items-start mr-auto text-left'>Fahombo</CardTitle>
-            <p className='items-end ml-auto text-justify text-xs lg:text-base'>Fahombo atau hombo merupakan tradisi lompat batu yang berasal dari Suku Nias. Tradisi Fahombo ini sudah diwariskan secara turun-temurun dan merupakan suatu kebanggaan tersendiri 
-            bagi masyarakat Suku Nias. Fahombo ini diwariskan kepada anak laki-laki, mereka telah dilatih sejak kecil. Fahombo dilakukan oleh para pemuda laki-laki dengan cara melompati tumpukan batu kurang lebih tingginya 2 hingga 2,2 meter 
-            tanpa menyentuh permukaan batu sedikitpun untuk menunjukkan bahwa mereka tangguh dan pantas dianggap dewasa secara fisik. Awalnya Fahombo ini merupakan kebiasaan berperang antar desa suku-suku di pulau Nias. Sebagai ritual bagi para pria 
-            suku Nias yang akan menjadi seseorang prajurit. Ketika pria berhasil melompati susunan batu tersebut, baru dia boleh ikut berperang. Sekarang Fahombo dipertunjukkan sebagai atraksi dan simbol budaya Suku Nias untuk mengisi acara yang
-            biasanya ditampilkan bersama atraksi perang.
-            </p>
+          <CardContent className='m-3 lg:m-10 flex flex-col justify-end items-start ml-auto lg:w-1/2 lg:order-1'>
+            <CardTitle className='text-base lg:text-3xl lg:ml-auto text-center lg:text-left'>Fahombo</CardTitle>
+            <p className='items-end ml-auto text-justify text-xs lg:text-base'>Fahombo atau hombo merupakan tradisi lompat batu yang berasal dari Suku Nias. Tradisi Fahombo ini sudah diwariskan secara turun-temurun dan merupakan suatu kebanggaan tersendiri bagi masyarakat Suku Nias. Fahombo ini diwariskan kepada anak laki-laki, mereka telah dilatih sejak kecil. Fahombo dilakukan oleh para pemuda laki-laki dengan cara melompati tumpukan batu kurang lebih tingginya 2 hingga 2,2 meter tanpa menyentuh permukaan batu sedikitpun untuk menunjukkan bahwa mereka tangguh dan pantas dianggap dewasa secara fisik. Awalnya Fahombo ini merupakan kebiasaan berperang antar desa suku-suku di pulau Nias. Sebagai ritual bagi para pria suku Nias yang akan menjadi seseorang prajurit. Ketika pria berhasil melompati susunan batu tersebut, baru dia boleh ikut berperang. Sekarang Fahombo dipertunjukkan sebagai atraksi dan simbol budaya Suku Nias untuk mengisi acara yang biasanya ditampilkan bersama atraksi perang.</p>
           </CardContent>
         </Card>
 
@@ -153,49 +144,49 @@ export default function Budaya() {
         data-aos="fade-up"
       >
         <div className='swiper-button-prev text-krem bg-hijau1'></div>
-            <SwiperSlide className='w-1/2'>
+            <SwiperSlide className=' w-3/4 lg:w-4/6'>
               <div className="relative group overflow-hidden rounded-t-2xl">
                 <img src={gambarBajuBatakToba} className='object-cover w-full h-full'/>
                 <p className={styleHoverSlider}>Menggunakan kain tenunan yang khas yaitu Ulos. Umumnya warna benang yang terdapat pada Ulos ada putih, emas, merah, hitam ataupun perak. 
                 Untuk pakaian pria, bagian atasnya disebut Ampe-Ampe dan bagian bawah disebut Singkot. Sedangkan untuk pakaian perempuan, bagian atas disebut Hoba-Hoba dan bagian bawah disebut Haen.</p>
               </div>
-              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-xl">Suku Batak Toba</h1>
+              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-2xl">Suku Batak Toba</h1>
             </SwiperSlide>
-            <SwiperSlide className='w-1/2'>
+            <SwiperSlide className=' w-3/4 lg:w-4/6'>
               <div className="relative group overflow-hidden rounded-t-2xl">
                 <img src={gambarBajuKaro} className='object-cover w-full h-full'/>
                 <p className={styleHoverSlider}>Menggunakan kain tenun khusus yang terbuat dari pintalan kapas disebut dengan Uis Gara. Proses pembuatan Uis Gara menggunakan benang berwarna merah yang 
                 kadang dipadukan dengan warna hitam dan putih serta dihiasi tenunan benang warna emas dan perak. Biasanya Uis Gara digunakan saat kegiatan sehari-hari dan upacara adat. Cara pengunaannya 
                 juga berbeda-beda bergantung pada acaranya, oleh karena itu tiap-tiap penggunaan memiliki makna dan simbol yang berbeda. </p>
               </div>
-              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-xl">Suku Karo</h1>
+              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-2xl">Suku Karo</h1>
             </SwiperSlide>
-            <SwiperSlide className='w-1/2'>
+            <SwiperSlide className=' w-3/4 lg:w-4/6'>
               <div className="relative group overflow-hidden rounded-t-2xl">
                 <img src={gambarBajuPakpak} className='object-cover w-full h-full'/>
                 <p className={styleHoverSlider}>Menggunakan kain tenun khas yang bernama Oles. Pakaian adat Suku Pakpak didominasi oleh warna hitam yang dibuat dengan bahan dasar katun dan perpaduan kain Oles, 
                 biasanya pakaian ini disebut sebagai baju merapi-api. Pada laki-laki pakaian adatnya disebut Borgot yang dipadukan dengan celana hitam dan sarung Sidosdos, sedangkan pada perempuan pakaian adatnya 
                 disebut Cimata yang dipadukan dengan sarung Oles Perdabaitak yang dililitkan pada pinggang dengan melingkar. </p>
               </div>
-              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-xl">Suku Pak Pak</h1>
+              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-2xl">Suku Pak Pak</h1>
             </SwiperSlide>
-            <SwiperSlide className='w-1/2'>
+            <SwiperSlide className=' w-3/4 lg:w-4/6'>
               <div className="relative group overflow-hidden rounded-t-2xl">
                 <img src={gambarBajuMelayu} className='object-cover w-full h-full'/>
                 <p className={styleHoverSlider}>Seperti kebanyakan pakaian adat Melayu, pakaian adat Suku Melayu mengenakan Baju Kurung yang dilengkapi oleh balutan Kain Songket. Untuk perempuan, menggunakan 
                 Baju Kurung atau Kebaya Panjang dipadukan dengan Songket. Bahan dari Baju Kurung khusus perempuan ini terbuat dari Kain Brokat atau Kain Sutra. Sementara pada laki-laki menggunakan Teluk Balaga 
                 yang dipadukan dengan Tengkulok dan Lengas sebagai lambang kebesaran dan kegagahan.</p>
               </div>
-              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-xl">Suku Melayu</h1>
+              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-2xl">Suku Melayu</h1>
             </SwiperSlide>
-            <SwiperSlide className='w-1/2'>
+            <SwiperSlide className=' w-3/4 lg:w-4/6'>
               <div className="relative group overflow-hidden rounded-t-2xl">
                 <img src={gambarBajuAngkola} className='object-cover w-full h-full'/>
                 <p className={styleHoverSlider}>Menggunakan bahan dasar Kain Ulos dengan dominasi warna hitam juga perpaduan warna merah. Pakaian suku Angkola memiliki kemiripan dengan pakaian Suku Mandailing, 
                 namun perbedaannya terletak pada pakaian perempuan yang didominasi warna merah dipadukan dengan selendang yang diselempangkan dan pada pakaian laki-laki, hiasan kepalanya serupa dengan Suku Mandailing 
                 namun dengan aksesoris tambahan penutup kepala yang disebut Ampu. </p>
               </div>
-              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-xl">Suku Angkola</h1>
+              <h1 className="flex justify-center bg-krem rounded-b-2xl lg:text-2xl">Suku Angkola</h1>
             </SwiperSlide>
         <div className='swiper-button-next text-krem bg-hijau1'></div>
         </Swiper>
@@ -204,53 +195,53 @@ export default function Budaya() {
         <hr className="m-5 border border-b-2 w-1/4 mx-auto text-sm lines" data-aos="fade-up"/>
         <div className='flex flex-row flex-wrap justify-center space-y' data-aos='fade-up'>
           <Card className={getCardClasses('gordang')} onClick={() => handleCardClick('gordang')}>
+            <div className='w-full'>
+              <img src={gambarGordangSambilan} className='rounded-t-2xl object-cover w-full h-full cursor-pointer'/>
+            </div>
             <CardContent className={styleMusic}>
                 <CardTitle className='flex items-center justify-center text-lg lg:text-xl'>Gordang Sambilan</CardTitle>
                 <p className="text-justify text-xs lg:text-base">Berasal dari Suku Mandailing. Terdiri atas 9 gendang dimana setiap gendang memiliki ukuran dan panjang diameter yang berbeda sehingga karakter suara yang berbeda. Gordang Sambilan dimainkan saat ada upacara adat misal seperti pernikahan atau pemakaman diperlukan bisa sampai 6 orang untuk memainkannya.</p>
             </CardContent>
-            <div className='w-full'>
-              <img src={gambarGordangSambilan} className='rounded-b-2xl object-cover w-full h-full cursor-pointer'/>
-            </div>
           </Card>
 
           <Card className={getCardClasses('taganing')} onClick={() => handleCardClick('taganing')}>
+            <div className='w-full'>
+              <img src={gambarTaganing} className='rounded-t-2xl object-cover w-full h-full cursor-pointer'/>
+            </div>
             <CardContent className={styleMusic}>
                 <CardTitle className='flex items-center justify-center text-lg lg:text-xl'>Taganing</CardTitle>
                 <p className="text-justify text-xs lg:text-base">Berasal dari Suku Batak Toba. Terdiri atas 5 gendang yang masing-masing memiliki nada yang berbeda. Dimainkan dengan cara dipukul dengan tangan atau alat khusus. Taganing memiliki peranan sangat penting dalam penyelenggaraan pertunjukan musik Gondang karena sebagai pilar utama pembentuk struktur musik Gondang.</p>
             </CardContent>
-            <div className='w-full'>
-              <img src={gambarTaganing} className='rounded-b-2xl object-cover w-full h-full cursor-pointer'/>
-            </div>
           </Card>
 
           <Card className={getCardClasses('faritia')} onClick={() => handleCardClick('faritia')}>
+            <div className='w-full'>
+              <img src={gambarFaritia} className='rounded-t-2xl object-cover w-full h-full cursor-pointer' />
+            </div>
             <CardContent className={styleMusic}>
                 <CardTitle className='flex items-center justify-center text-lg lg:text-xl'>Faritia</CardTitle>
                 <p className="text-justify text-xs lg:text-base">Berasal dari Suku Nias. Memiliki bentuk mirip dong dengan diameter yang lebih kecil sekitar 20cm hingga 30cm, ketebalannya hingga 4cm, dan bagian tengah yang menonjol. Dimainkan dengan cara dipukul menggunakan pemukul bagian tengah atau tepinya. Faritia biasanya dimainkan sebagai bagian dari ansambel musik khas Nias. </p>
             </CardContent>
-            <div className='w-full'>
-              <img src={gambarFaritia} className='rounded-b-2xl object-cover w-full h-full cursor-pointer' />
-            </div>
           </Card>
 
           <Card className={getCardClasses('balobat')} onClick={() => handleCardClick('balobat')}>
+            <div className='w-full'>
+              <img src={gambarBalobat} className='rounded-t-2xl object-cover w-full h-full cursor-pointer'/>
+            </div>
             <CardContent className={styleMusic}>
                 <CardTitle className='flex items-center justify-center text-lg lg:text-xl'>Balobat</CardTitle>
                 <p className="text-justify text-xs lg:text-base">Berasal dari Suku Karo. Berbentuk menyerupai seruling rekorder, namun dengan ukuran yang lebih kecil serta nada yang berbeda. Cara memainkan alat musik ini dengan meniup menggunakan mulut untuk menghasilkan suara melalui lubang-lubang di bambu. Balobat dapat dimainkan secara solo maupun dalam kelompok musik tradisional.</p>
             </CardContent>
-            <div className='w-full'>
-              <img src={gambarBalobat} className='rounded-b-2xl object-cover w-full h-full cursor-pointer'/>
-            </div>
           </Card>
 
           <Card className={getCardClasses('doli')} onClick={() => handleCardClick('doli')}>
+            <div className='w-full'>
+              <img src={gambarDolidoli} className='rounded-t-2xl object-cover w-full h-full cursor-pointer'/>
+            </div>
             <CardContent className={styleMusic}>
                 <CardTitle className='flex items-center justify-center text-lg lg:text-xl'>Doli-Doli</CardTitle>
                 <p className="text-justify text-xs lg:text-base">Berasal dari Suku Nias. Berbentuk menyerupai Kolintang, namun dengan ukuran yang lebih kecil. Cara memainkannya dipukul, ketika dipukul akan menghasilkan pola bunyi yang khas. Umumnya dimainkan sebagai sarana hiburan saat di ladang, dalam suasana dukacita/kesepian, dan upacara adat dalam konteks keagamaan. </p>
             </CardContent>
-            <div className='w-full'>
-              <img src={gambarDolidoli} className='rounded-b-2xl object-cover w-full h-full cursor-pointer'/>
-            </div>
           </Card>
         </div>
 
